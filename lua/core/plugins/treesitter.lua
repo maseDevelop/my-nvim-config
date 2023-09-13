21875d857c41bd92ad85606d9a6b3d75e224ba3d
mase-jarden <mason.elliott@jarden.co.nz>
2023-09-13 17:09:36 +1200

    chore: update spacing

--- lua/core/plugins/treesitter.lua
+++ lua/core/plugins/treesitter.lua
@@ -12,6 +12,7 @@ require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
   ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
 
+
   -- Autoinstall languages that are not installed.
   auto_install = true,
 
