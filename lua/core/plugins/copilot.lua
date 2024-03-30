a9b887dc26ade02541ba314cac53df56063617fa
maseDevelop <mason.elliott12@gmail.com>
2024-03-30 22:08:13 +1300

    update copilot based on lucidph3nx

--- lua/core/plugins/copilot.lua
+++ lua/core/plugins/copilot.lua
@@ -1,1 +1,43 @@
-return {'github/copilot'}
+return {
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  event = "InsertEnter",
+  opts = {
+    panel = {
+      enabled = false, -- disabled due to the keymap affecting insert mode
+      auto_refresh = false,
+      layout = {
+        position = "bottom", -- | top | left | right
+        ratio = 0.4
+      },
+      keymap = {
+        open = '<leader>cp',
+      },
+    },
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      debounce = 75,
+      keymap = {
+        accept = '<C-L>',
+        next = '<C-J>',
+        prev = '<C-K',
+      },
+    },
+    filetypes = {
+      markdown = true,
+      yaml = true,
+      help = false,
+      gitcommit = false,
+      gitrebase = false,
+      hgcommit = false,
+      svn = false,
+      cvs = false,
+      ["."] = true,
+      rust = false, -- while learning rust, no copilot
+    },
+  },
+  config = function(_, opts)
+    require('copilot').setup(opts)
+  end,
+}
