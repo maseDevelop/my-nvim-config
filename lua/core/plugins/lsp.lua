ec0fe82dc822e3c51e4d132309fdb4d0cac7c348
MaseDevelop <Mason.Elliott@Masons-MBP.lan>
2024-11-08 13:46:34 +1300

    add python back

--- lua/core/plugins/lsp.lua
+++ lua/core/plugins/lsp.lua
@@ -65,13 +65,33 @@ return {
 
     -- Enable the following language servers
     --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-    --
     --  Add any additional override configuration in the following tables. They will be passed to
     --  the `settings` field of the server config. You must look up that documentation yourself.
     --
     --  If you want to override the default filetypes that your language server will attach to you can
     --  define the property 'filetypes' to the map in question.
     local servers = {
+      pylsp = {
+        filetypes = { 'python' },
+        configurationSources = { 'flake8' },
+        plugins = {
+          pylint = {
+            enabled = true,
+          },
+          flake8 = {
+            maxLineLength = 100,
+          },
+          pyflakes = {
+            enabled = false,
+          },
+          pycodestyle = {
+            enabled = false,
+          },
+          rope_completion = {
+            enabled = true,
+          },
+        },
+      },
       ts_ls = {},
       lua_ls = {
         Lua = {
