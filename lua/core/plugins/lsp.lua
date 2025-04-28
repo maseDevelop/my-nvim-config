2bf7557db35d12ccbbf27101e4a8935855fb471b
maseDevelop <Mason.Elliott@Masons-MacBook-Pro-2.local>
2025-04-29 11:08:14 +1200

    add metals

--- lua/core/plugins/lsp.lua
+++ lua/core/plugins/lsp.lua
@@ -7,48 +7,30 @@ return {
     'williamboman/mason-lspconfig.nvim',
 
     -- Useful status updates for LSP
-    -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-    { 'j-hui/fidget.nvim',       tag = 'legacy', opts = {} },
+    { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
 
     -- Additional lua configuration, makes nvim stuff amazing!
     'folke/neodev.nvim',
-
-    -- Scala metals
-    { 'scalameta/nvim-metals', dependencies = { 'nvim-lua/plenary.nvim' } }
   },
   config = function()
     -- [[ Configure LSP ]]
-    --  This function gets run when an LSP connects to a particular buffer.
     local on_attach = function(_, bufnr)
-      -- NOTE: Remember that lua is a real programming language, and as such it is possible
-      -- to define small helper and utility functions so you don't have to repeat yourself
-      -- many times.
-      --
-      -- In this case, we create a function that lets us more easily define mappings specific
-      -- for LSP related items. It sets the mode, buffer and description for us each time.
       local nmap = function(keys, func, desc)
         if desc then
           desc = 'LSP: ' .. desc
         end
-
         vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
       end
 
       nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
       nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
       nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
       nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
       nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
       nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
       nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
       nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
-      -- See `:help K` for why this keymap
       nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-      -- nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-      -- Lesser used LSP functionality
       nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
       nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
       nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
@@ -58,43 +40,49 @@ return {
     end
 
     -- Enable the following language servers
-    --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-    --  Add any additional override configuration in the following tables. They will be passed to
-    --  the `settings` field of the server config. You must look up that documentation yourself.
-    --
-    --  If you want to override the default filetypes that your language server will attach to you can
-    --  define the property 'filetypes' to the map in question.
     local servers = {
       pylsp = {
         filetypes = { 'python' },
         configurationSources = { 'flake8' },
         plugins = {
-          pylint = {
-            enabled = true,
-          },
-          flake8 = {
-            maxLineLength = 100,
-          },
-          pyflakes = {
-            enabled = false,
-          },
-          pycodestyle = {
-            enabled = false,
-          },
-          rope_completion = {
-            enabled = true,
-          },
+          pylint = { enabled = true },
+          flake8 = { maxLineLength = 100 },
+          pyflakes = { enabled = false },
+          pycodestyle = { enabled = false },
+          rope_completion = { enabled = true },
         },
       },
       terraformls = {},
       jdtls = {},
-      ts_ls = {},
+      tsserver = {},
       lua_ls = {
         Lua = {
           workspace = { checkThirdParty = false },
           telemetry = { enable = false },
         },
-      }
+      },
+      metals = {
+        cmd = { "metals" },
+        filetypes = { "scala" },
+        root_dir = require('lspconfig.util').root_pattern(
+          "build.sbt",
+          "build.sc",
+          "build.gradle",
+          "pom.xml"
+        ),
+        init_options = {
+          compilerOptions = {
+            snippetAutoIndent = false,
+          },
+          isHttpEnabled = true,
+          statusBarProvider = "show-message",
+        },
+        settings = {
+          workspace = {
+            configuration = false,
+          },
+        },
+      },
     }
 
     -- Setup neovim lua configuration
@@ -113,13 +101,19 @@ return {
 
     mason_lspconfig.setup_handlers {
       function(server_name)
-        require('lspconfig')[server_name].setup {
+        local server_config = {
           capabilities = capabilities,
           on_attach = on_attach,
-          settings = servers[server_name],
-          filetypes = (servers[server_name] or {}).filetypes,
         }
-      end
+
+        if servers[server_name] then
+          for key, value in pairs(servers[server_name]) do
+            server_config[key] = value
+          end
+        end
+
+        require('lspconfig')[server_name].setup(server_config)
+      end,
     }
   end,
 }
