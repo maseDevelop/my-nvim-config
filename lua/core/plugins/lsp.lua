d0751a532af3f8891dc168f72534dcb2126687a4
maseDevelop <mason.elliott12@gmail.com>
2025-07-16 14:03:00 +1200

    update lsp

--- lua/core/plugins/lsp.lua
+++ lua/core/plugins/lsp.lua
@@ -1,97 +1,68 @@
 return {
-  -- LSP Configuration & Plugins
-  'neovim/nvim-lspconfig',
-  dependencies = {
-    -- Automatically install LSPs to stdpath for neovim
-    { 'williamboman/mason.nvim', config = true },
-    'williamboman/mason-lspconfig.nvim',
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
+    },
+    config = function()
+        -- [[ Configure LSP ]]
+        local on_attach = function(_, bufnr)
+            local nmap = function(keys, func, desc)
+                if desc then
+                    desc = 'LSP: ' .. desc
+                end
+                vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+            end
 
-    -- Useful status updates for LSP
-    { 'j-hui/fidget.nvim',       tag = 'legacy', opts = {} },
-
-    -- Additional lua configuration, makes nvim stuff amazing!
-    'folke/neodev.nvim',
-  },
-  config = function()
-    -- [[ Configure LSP ]]
-    local on_attach = function(_, bufnr)
-      local nmap = function(keys, func, desc)
-        if desc then
-          desc = 'LSP: ' .. desc
+            nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+            nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+            nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+            nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+            nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+            nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+            nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+            nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+            nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+            nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
         end
-        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-      end
 
-      nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-      nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-      nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-      nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-      nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-      nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-      nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-      nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-      nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-      nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-      nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-      nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-      nmap('<leader>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-      end, '[W]orkspace [L]ist Folders')
-    end
+        vim.api.nvim_create_autocmd('LspAttach', {
+            callback = function(args)
+                local bufnr = args.buf
+                on_attach(_, bufnr)
+            end,
+        })
 
-    -- Enable the following language servers
-    local servers = {
-      pylsp = {
-        filetypes = { 'python' },
-        configurationSources = { 'flake8' },
-        plugins = {
-          pylint = { enabled = true },
-          flake8 = { maxLineLength = 100 },
-          pyflakes = { enabled = false },
-          pycodestyle = { enabled = false },
-          rope_completion = { enabled = true },
-        },
-      },
-      terraformls = {},
-      jdtls = {},
-      ts_ls = {},
-      lua_ls = {
-        Lua = {
-          workspace = { checkThirdParty = false },
-          telemetry = { enable = false },
-        },
-      },
-    }
 
-    -- Setup neovim lua configuration
-    require('neodev').setup()
-
-    -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
-    -- Ensure the servers above are installed
-    local mason_lspconfig = require 'mason-lspconfig'
-
-    mason_lspconfig.setup {
-      ensure_installed = vim.tbl_keys(servers),
-    }
-
-    mason_lspconfig.setup {
-      function(server_name)
-        local server_config = {
-          capabilities = capabilities,
-          on_attach = on_attach,
+        -- Enable the following language servers
+        local servers = {
+            pylsp = {
+                filetypes = { 'python' },
+                configurationSources = { 'flake8' },
+                plugins = {
+                    pylint = { enabled = true },
+                    flake8 = { maxLineLength = 100 },
+                    pyflakes = { enabled = false },
+                    pycodestyle = { enabled = false },
+                    rope_completion = { enabled = true },
+                },
+            },
+            terraformls = {},
+            jdtls = {},
+            ts_ls = {},
+            lua_ls = {
+                Lua = {
+                    workspace = { checkThirdParty = false },
+                    telemetry = { enable = false },
+                },
+            },
         }
 
-        if servers[server_name] then
-          for key, value in pairs(servers[server_name]) do
-            server_config[key] = value
-          end
-        end
+        require("mason-lspconfig").setup({
+            automatic_enable = true,
+            ensure_installed = servers,
+        })
+    end,
 
-        require('lspconfig')[server_name].setup(server_config)
-      end,
-    }
-  end,
 }
